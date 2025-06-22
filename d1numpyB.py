36171d7b33baa4b77b0fa529ed692f906abc84f6
krrish-exe <krishsathvara7383@gmail.com>
2025-06-22 21:51:14 +0530

    here i added new file for day 1

---
+++ d1numpyB.py
@@ -0,0 +1,33 @@
+''' 
+what is numpy ?
+numpy is a python library used for working with arrays
+it also has functions for working in domain of linear algebra, fourier transform, and matrices
+Numpy was created by the travis oliphant in 2005 . it is open source project and you can also use it freely
+numpy standsb for numerical python
+
+In Python we have lists that serve the purpose of arrays, but they are slow to process.
+NumPy aims to provide an array object that is up to 50x faster than traditional Python lists.
+The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
+
+Data Science: is a branch of computer science where we study how to store, use and analyze data for deriving information from it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
