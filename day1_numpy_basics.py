8da882570842794932e92a5d34258cb2607ddbde
krrish-exe <krishsathvara7383@gmail.com>
2025-06-23 23:26:16 +0530

    Learning numpy example code

--- day1_numpy_basics.py
+++ day1_numpy_basics.py
@@ -16,18 +16,28 @@ Data Science: is a branch of computer science where we study how to store, use a
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''
+import numpy as np
+
+arr= np.array([1,2,3,4,5,6,7,8])
+print (arr)
+print(type(arr))
+# use tuple to create qa array
+arrtuple=np.array((1,2,3,4,5,6))
+print(arrtuple)
+
+
+#Learning by diurectly doing challenges
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([3, 4, 5, 6, 7])
+print(arr1)
+print(arr2)
+
+
+print (arr1- arr2 , "substraction")
+print (arr1+ arr2 , "addition")
+print(arr1*arr2,"multiplication")
+print(arr1/arr2,"division")
