6556261192be1d46ef82782efd126a17d3e45997
krrish-exe <krishsathvara7383@gmail.com>
2025-07-18 01:47:52 +0530

    nothing

--- practice.py
+++ practice.py
@@ -6,3 +6,15 @@ print(df)
 #see First 5 rows 
 
 print (df.head())
+
+# Let’s pick just a few clean columns to reshape, like:
+df_small =df[['title','type','release_year', 'rating']]
+print(df_small.head())
+
+#melt
+melted= pd.melt(df_small,
+    id_vars=['title', 'type'],       # Keep title & type fixed
+    var_name='Feature',              # Name of new column for melted column names
+    value_name='Value'               # Name of new column for melted values
+)
+print(melted.head(10))
